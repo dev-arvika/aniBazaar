6d62ea895411c199a3a933ef62aac6d81933a696
unknown <dev.arvika@gmail.com>
2024-10-24 13:00:30 +0530

    db cred changes and other

--- src/main/java/com/ani/bazaar/entity/UserEntity.java
+++ src/main/java/com/ani/bazaar/entity/UserEntity.java
@@ -3,7 +3,6 @@ package com.ani.bazaar.entity;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
@@ -16,7 +15,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
